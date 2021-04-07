3df87e3a1986c68b26543bf6ffeba817b81e5400
Petr Kamenev <75952058+Pitonixx@users.noreply.github.com>
2021-04-07 11:40:44 +0300

    Add files via upload

--- domname.py
+++ domname.py
@@ -1,24 +1,24 @@
 from sys import argv
-import socket
+import sys,socket
 
 output=[]
+domain_name=argv
+y = len(domain_name)
 
-for n in range(1,4):
-	domain_name = argv[n]
-	ip = socket.gethostbyname(domain_name)
+for n in range(1,y):
+	a = domain_name[n]
+	ip = socket.gethostbyname(a)
 	output.insert(n,ip)
 
 for x in output:
-        print("__"+x+"__")
+        print(" __ "+x+"__")
 
 
-for i in range(0,5):
-	for n in range(1,4):
-		domain_name = argv[n]
-		ip = socket.gethostbyname(domain_name)
-		print(domain_name+" "+ip)
-		test = output[n]
+while y > 1:
+	for n in range(1,y):
+		ip = socket.gethostbyname(domain_name[n])
+		print(domain_name[n]+" "+ip)
+		test = output[n-1]
 		if test != ip:
-			print("[ERROR] "+domain_name+" IP mismatch: "+output[n]+" "+ip)
-			break
+			print("[ERROR] "+domain_name[n]+" IP mismatch: "+output[n]+" "+ip)
 		output.insert(n,ip)
